f82ce7e40d9af44482601af909071c8039e59709
daniel tea <daniel.l.tea@gmail.com>
2023-05-22 14:50:13 -0700

    use project references

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import { my_merge } from '../../utils/src/index'
 
 const app = express()
 const port = 3000
+
+my_merge({}, { test: 'my message' })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
