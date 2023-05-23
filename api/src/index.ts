3e8ccc9b80ce37a57288207b7736f366413c856b
daniel tea <daniel.l.tea@gmail.com>
2023-05-22 17:53:11 -0700

    try internal packages

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,5 @@
 import express from 'express'
 import { FOO, my_merge } from '@qwert/utils'
-// import { FOO, my_merge } from '@qwert/test'
-// import { my_merge } from '@scope/name'
 
 const app = express()
 const port = 3000
